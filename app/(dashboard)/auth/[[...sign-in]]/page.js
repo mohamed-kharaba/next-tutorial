6fde392e4c6c58074e33726fa25f950fcb5e0908
mohamed kharaba <m.kharaba@zadgroup.net>
2024-01-15 09:21:02 +0300

    Add Route

---
+++ app/(dashboard)/auth/[[...sign-in]]/page.js
@@ -0,0 +1,5 @@
+const SignInPage = ({ params }) => {
+    console.log(params);
+    return <div>SignInPage</div>;
+};
+export default SignInPage;
--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,4 +1,24 @@
-const DrinkPage = () => {
-    return <h1 className="text-7xl">DrinkPage</h1>;
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
+import DrinksList from "@/components/DrinksList";
+
+const fetchDrinks = async () => {
+    // just for demo purposes
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const response = await fetch(url);
+    // throw error
+    if (!response.ok) {
+        throw new Error("Failed to fetch drinks...");
+    }
+    const data = await response.json();
+    return data;
+};
+
+const DrinkPage = async () => {
+    const data = await fetchDrinks();
+    return (
+        <div>
+            <DrinksList drinks={data.drinks} />
+        </div>
+    );
 };
 export default DrinkPage;
